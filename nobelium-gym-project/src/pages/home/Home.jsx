9e00bea4a5a1ff7ba13a3eb07d4c836ca69204fd
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-04-19 13:41:17 +0530

    Home Page

--- nobelium-gym-project/src/pages/home/Home.jsx
+++ nobelium-gym-project/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import Header from '../../components/mainHeader/MainHeader';
 import Programs from '../../components/programs/Programs';
 import Values from '../../components/values/Values';
 import FAQsection from '../../components/FAQ/FAQsection';
-import Testimonials from '../../components/testimonials/Testimonials';
+import Testimonials from '../../components/testimonials/Testimonials'
 
 const Home = () => {
   return (
