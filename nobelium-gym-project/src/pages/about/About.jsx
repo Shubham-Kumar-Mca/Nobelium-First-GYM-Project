e3bda3598f1c6fb2f802cf01139014432ebdc23b
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-04-19 13:40:29 +0530

    About Section

--- nobelium-gym-project/src/pages/about/About.jsx
+++ nobelium-gym-project/src/pages/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import "./about.css"
-import HeaderImage from "../../../images/header_bg_1.jpg"
+import "./about.css";
+import HeaderImage from "../../../images/header_bg_1.jpg";
 import Header from '../../components/header/Header';
 
 const About = () => {
