633444ed538c7e4c781dc410ab4d73ca724270d0
robin <rcgaur84@gmail.com>
2023-04-20 11:06:45 +0530

    Removing not required comments

--- nobelium-gym-project/src/App.jsx
+++ nobelium-gym-project/src/App.jsx
@@ -1,23 +1,12 @@
 
 import AboutUs from './pages/AboutUs/AboutUs'
-import RegisterLogin from './pages/RegisterLogin/RegisterLogin'
-
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
 
 function App() {
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={'/about'} element={<AboutUs />} />
-        <Route path={'/'} element={<RegisterLogin />} />
-      </Routes>
-    </BrowserRouter>
-    // <div>
-    //   {/* <AboutUs /> */}
-    //   <RegisterLogin/>
-    // </div>
+  return(
+    <div>
+      <AboutUs />
+    </div>
   )
 }
 
